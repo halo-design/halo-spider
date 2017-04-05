0a97dcbc205b17dd4e67d2e0a0572a2fe4699acd
Aford <244734834@qq.com>
2017-04-05 13:52:11 +0800

    optmz

--- bin/download.js
+++ bin/download.js
@@ -1,9 +1,8 @@
-const fs = require('fs')
 const utils = require('./utils')
 
 utils.downloader({
   outputPath: './assets/wallhaven-girls',
-  downloadQueue: JSON.parse(fs.readFileSync('./data/wallhaven-girls.json')).src,
+  downloadQueue: utils.readJSON('./data/wallhaven-girls.json').src,
   thread: 15,
   startIndex: 0,
   endIndex: 30
---
+++ bin/jandan-spider.js
@@ -0,0 +1,33 @@
+const colors = require('colors')
+const async = require('async')
+const utils = require('./utils')
+
+
+let allImgURL = []
+let failedCount = 0
+
+
+const PAGES_URL = utils.URLgenerator('http://jandan.net/ooxx/page-{{index}}#comments', 2400, 2422)
+
+async.mapLimit(
+  PAGES_URL,
+  50,
+
+  (url, cb) => utils.getContent(
+    url,
+    null,
+    cb,
+    data => utils.extractURL(data, '.commentlist .row .text p a', 'href', url => allImgURL.push(url))
+  ),
+
+  (error, result) => {
+    console.log(`Get ${failedCount} images link failed!`.red)
+    console.log(`Get ${allImgURL.length - failedCount} images link successfully!`.green)
+    utils.writeJSON('./data/jandan.json', {src: allImgURL})
+    utils.downloader({
+      outputPath: './assets/jandan',
+      downloadQueue: allImgURL,
+      thread: 15
+    })
+  }
+)
